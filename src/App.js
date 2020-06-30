db10c2e2298a6a33c6d0b1a8b96d40b22bce6bd9
Louislu666 <nt9510louis@163.com>
2020-06-30 17:42:14 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+/* 
+  1.引入sass文件默认的后缀名.scss
+*/
+import './App.scss';
+import Icon from "./Icon";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>标题</h1>
+    </div>
+  );
+}
+
+export default App;
